8670a3749104fcf86514e92e08941e5241863730
Brandy La Voy <brandylavoy@gmail.com>
2017-06-06 21:53:02 -0700

    capitalized the T in header

--- public/components/header.js
+++ public/components/header.js
@@ -1,16 +1,17 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 
 
 export default class Header extends React.Component {
-    render(){
-        return (
-            <div>
-                <h1><img className="logo" src="./assets/images/Bart-logo.svg" />
-                Search BART trips</h1>
-            </div>
-            );
+    render() {
+        return ( <
+            div >
+            <
+            h1 > < img className = "logo"
+            src = "./assets/images/Bart-logo.svg" / >
+            Search BART Trips < /h1> <
+            /div>
+        );
     }
 }
